f298363468defa585b38e4c59cd1cb10a7de7ee4
aleks.haler <aleks.haler.work@gmail.com>
2020-05-24 00:03:51 +0300

    half first timeline

---
+++ Assets/stopTimeline.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class stopTimeline : MonoBehaviour
+{
+    public PlayableDirector director;
+    public Animator Aaldosterone_level_right;
+    public Animator Male;
+
+    void OnEnable()
+    {
+        Aaldosterone_level_right.SetTrigger("TriggerFlash");
+        Male.SetTrigger("TriggerFlash");
+        director.Pause();
+    }
+
+    public void UnPause()
+    {
+        Aaldosterone_level_right.ResetTrigger("TriggerFlash");
+        Male.ResetTrigger("TriggerFlash");
+        director.Resume();
+    }
+    
+}
