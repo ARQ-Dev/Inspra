6fdaf33d89d20273c6efa58dec66df9a4025df4d
Denis <yansubd@gmail.com>
2020-06-04 07:03:27 +0300

    default pattern

---
+++ Assets/Scripts/UI/Popup/Popup.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ARQ.NeuroSym.UIKit
+{
+    [RequireComponent(typeof(RectTransform))]
+    public class Popup : MonoBehaviour
+    {
+        [SerializeField]
+        private float _toastDuration;
+        [SerializeField]
+        private float _moveDuration;
+        [SerializeField]
+        private GameObject[] _popups;
+        [SerializeField]
+        private Vector2 _shownPosition;
+        [SerializeField]
+        private Vector2 _hidenPosition;
+
+        private RectTransform _rectTransfrom;
+        private GameObject _currentPopup;
+
+        private void Awake()
+        {
+            _rectTransfrom = GetComponent<RectTransform>();
+        }
+
+        public void ShowPopupByIndex(int index)
+        {
+            if(_currentPopup != null)
+            {
+                Destroy(_currentPopup);
+                _currentPopup = null;
+            }
+            _currentPopup = Instantiate(_popups[index], transform);
+            ShowPopup();
+        }
+
+        private void ShowPopup()
+        {
+            StopAllCoroutines();
+            _rectTransfrom.DOAnchorPos(_shownPosition, _moveDuration).SetEase(Ease.OutExpo);
+            StartCoroutine( StartCountdownCoroutine() );
+        }
+        private void HidePopup()
+        {
+            _rectTransfrom.DOAnchorPos(_hidenPosition, _moveDuration).SetEase(Ease.OutExpo);
+        }
+
+        private IEnumerator StartCountdownCoroutine()
+        {
+            yield return new WaitForSecondsRealtime(_toastDuration);
+            HidePopup();
+        }
+
+    }
+}
