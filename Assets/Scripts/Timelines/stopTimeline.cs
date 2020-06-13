b67ef2206d3fbc0cd664ed52458dc74abcf9aaa0
aleks.haler <aleks.haler.work@gmail.com>
2020-06-13 03:41:03 +0300

    TextFixes1

--- Assets/Scripts/Timelines/stopTimeline.cs
+++ Assets/Scripts/Timelines/stopTimeline.cs
@@ -8,24 +8,27 @@ public class stopTimeline : MonoBehaviour
     public PlayableDirector director;
     public Animator Male;
     public Animator Level;
+    public Animator Solve;
     public AudioSource audioSource;
     void OnEnable()
     {
         director.playableGraph.GetRootPlayable(0).SetSpeed(0);
         Male.Play("MaleFlash");
         Level.Play("IndicatorFlash");
-
+        Solve.Play("SolveProblemAppearing");
 
     }
 
     public void UnPause()
     {
         director.playableGraph.GetRootPlayable(0).SetSpeed(1);
+        Solve.Play("OneBigAnim");
     }
 
     public void Pause()
     {
         director.playableGraph.GetRootPlayable(0).SetSpeed(0);
+
     }
     
 }
